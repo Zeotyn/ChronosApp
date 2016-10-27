236a20d3cbb76024acb7941d101c0445e25b5171
Zeotyn <dev@zeotyn.de>
2016-10-27 16:52:47 +0200

    Initial commit

---
+++ ChronosAppTests/ChronosAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChronosAppTests.swift
+//  ChronosAppTests
+//
+//  Created by Eugen Waldschmidt on 27.10.16.
+//  Copyright © 2016 Eugen Waldschmidt. All rights reserved.
+//
+
+import XCTest
+@testable import ChronosApp
+
+class ChronosAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
